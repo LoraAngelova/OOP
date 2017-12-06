6a600e8b67d0e7ffb1383f5bb8ef5032490a4e12
LoraAngelova <33891380+LoraAngelova@users.noreply.github.com>
2017-12-06 09:51:25 +0200

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameQueue = new Queue1();
+            string name = "a";
+            while (name != string.Empty)
+            {
+                name = Console.ReadLine();
+                nameQueue.Push(name);
+            }
+
+            while (!nameQueue.Empty)
+            {
+                string names = nameQueue.Pop();
+                Console.WriteLine(names);
+            }
+        }
+    }
+}
--- Queue.cs
+++ Queue.cs
@@ -4,30 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace dvspisuk
-{
-    class Queue
-    {
-        private List<string> cont;
-        public void Push(string value)
-        {
-            this.cont.Add(value);
-        }
-        public string Pop()
-        {
-            string popped = cont[0];
-            cont.RemoveAt(0);
-            return popped;
-        }
-        public bool Empty
-        {
-            get { return cont.Count == 0; }
-        
-        }
-        public Queue()
-        {
-            this.cont = new List<string>();
-        }
 
-    }
-}
+    public class Queue1
+    {
+            private List<string> cont;
+            public void Push(string value)
+            {
+                this.cont.Add(value);
+            }
+            public string Pop()
+            {
+                string popped = cont[0];
+                cont.RemoveAt(0);
+                return popped;
+            }
+            public bool Empty
+            {
+                get { return cont.Count == 0; }
+
+            }
+            public Queue1()
+            {
+                this.cont = new List<string>();
+            }
+
+        }
